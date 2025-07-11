fe1070118054c2c1d2b7a6282e1a6367d2a2830d
Mandle <mculle2@gmail.com>
2025-07-10 22:15:17 -0500

    inventory

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,19 +1,28 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections.Generic;
+using static UnityEditor.Progress;
 
 public class Player : MonoBehaviour
 {
-
     public float moveSpeed;
     public Rigidbody2D rb2d;
     private Vector2 _movementInput;
     private bool insideNPCZone = false;
+    private List<IItem> Inventory;
+
+    private void Start()
+    {
+
+        Inventory = new List<IItem>(); 
+    }
 
     void Update()
     {
         transform.position += (Vector3)(moveSpeed * Time.fixedDeltaTime * _movementInput);
     }
 
+    //check if within distance of an NPC
     private void OnTriggerEnter2D(UnityEngine.Collider2D collision)
     {
         if (collision.gameObject.CompareTag("NPC"))
@@ -36,11 +45,27 @@ public class Player : MonoBehaviour
     {
         _movementInput = inputValue.Get<Vector2>().normalized;
     }
+
     public void OnInteract(InputValue inputValue)
     {
         if (insideNPCZone)
         {
-            Debug.Log("interacted with npc");
+            AddApple();
+            Debug.Log("ItemApple added to Inventory");
+            PrintInventory();
         }
+    }
+
+    public void PrintInventory()
+    {
+        for (int i = 0; i < Inventory.Count; i++)
+        {
+            Debug.Log("Inventory slot " + i + " contains " + Inventory[i]);
+        }
+    }
+    public void AddApple()
+    {
+        ItemApple apple = new ItemApple();
+        Inventory.Add(apple);
     }
 }
