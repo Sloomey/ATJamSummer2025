0a210fd2f4918390dbfc5a5d49ec0ad6cb12960a
Mandle <mculle2@gmail.com>
2025-07-13 23:34:20 -0500

    E over NPC heads

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -1,16 +1,55 @@
+using TMPro;
 using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class NPC : MonoBehaviour
 {
+
+    public GameObject floatingTextPrefab;
+    private GameObject floatingTextInstance;
+    private TextMeshProUGUI textComponent;
     public TextAsset dialogue;
+
+
+    void Update()
+    {
+        if (floatingTextInstance)
+        {
+            // Follow the GameObject's position
+            floatingTextInstance.transform.position = transform.position + Vector3.up * 2f;
+
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        textComponent.text = "E";
+
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        textComponent.text = "";
+
+    }
 
     private void Start()
     {
-        ChangeDialogueScript.SwitchDialogue += SwitchDialogue;
+        // Instantiate floating text above this GameObject
+        floatingTextInstance = Instantiate(floatingTextPrefab, transform.position, Quaternion.identity);
+        textComponent = floatingTextInstance.GetComponentInChildren<TextMeshProUGUI>();
+
+        // Set the name
+        textComponent.text = "";
+
+    ChangeDialogueScript.SwitchDialogue += SwitchDialogue;
     }
 
     private void OnDestroy()
     {
+        if (floatingTextInstance)
+        {
+            Destroy(floatingTextInstance);
+        }
         ChangeDialogueScript.SwitchDialogue -= SwitchDialogue;
     }
 
